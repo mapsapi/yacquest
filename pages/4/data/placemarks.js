a3fc0b2cab06aa27711967469a1836c413d5ab52
mapsapi <mstepanova@yandex-team.ru>
2013-07-30 17:13:40 +0400

    adding levels

---
+++ pages/4/data/placemarks.js
@@ -0,0 +1,75 @@
+function getPlacemarkParameters () {
+    return [{
+        coordinates: [55.72, 37.64],
+        properties: {
+            clusterCaption: 'Кафе "Хмурое утро"'
+        },
+        options: {
+            balloonIconImageHref: 'images/dot.jpg'
+        }
+    }, {
+        coordinates: [55.72, 37.64],
+        properties: {
+            clusterCaption: 'Кафе "Кризис жанра"'
+        },
+        options: {
+            balloonIconImageHref: 'images/t.jpg'
+        }
+    }, {
+        coordinates: [55.72, 37.64],
+        properties: {
+            clusterCaption: 'Автомагазин "Русь"'
+        },
+        options: {
+            balloonIconImageHref: 'images/b.jpg'
+        }
+    }, {
+        coordinates: [55.72, 37.64],
+        properties: {
+            clusterCaption: 'Магазин "Спортмастер"'
+        },
+        options: {
+            balloonIconImageHref: 'images/y.jpg'
+        }
+    }, {
+        coordinates: [55.72, 37.64],
+        properties: {
+            clusterCaption: 'Офис сбербанка'
+        },
+        options: {
+            balloonIconImageHref: 'images/dot.jpg'
+        }
+    }, {
+        coordinates: [55.72, 37.64],
+        properties: {
+            clusterCaption: 'Кафе "Шоколадница"'
+        },
+        options: {
+            balloonIconImageHref: 'images/l.jpg'
+        }
+    }, {
+        coordinates: [55.72, 37.64],
+        properties: {
+            clusterCaption: 'Пункт выдачи "Pichshop.ru"'
+        },
+        options: {
+            balloonIconImageHref: 'images/dot.jpg'
+        }
+    }, {
+        coordinates: [55.72, 37.64],
+        properties: {
+            clusterCaption: 'Аптека 36.6'
+        },
+        options: {
+            balloonIconImageHref: 'images/i.jpg'
+        }
+    }, {
+        coordinates: [55.72, 37.64],
+        properties: {
+            clusterCaption: 'Сервис-центр DAEWOO'
+        },
+        options: {
+            balloonIconImageHref: 'images/dot.jpg'
+        }
+    }];
+}
